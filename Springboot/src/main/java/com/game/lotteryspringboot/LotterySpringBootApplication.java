83377634f60eff68fc46ac0e78a47fe79a090257
lilfist <lilfist@gmail.com>
2023-11-15 02:32:38 -0500

    Initialize springboot startup

---
+++ Springboot/src/main/java/com/game/lotteryspringboot/LotterySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.game.lotteryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterySpringBootApplication.class, args);
+    }
+
+}
